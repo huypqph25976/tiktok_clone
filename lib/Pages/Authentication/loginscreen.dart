3da3ab96c0cdf966fcac86ff1194c60de0abb068
bfjshfbh <nhatltph28401@fpt.edu.vn>
2023-10-13 11:13:30 +0700

    nhat1

--- lib/Pages/Authentication/loginscreen.dart
+++ lib/Pages/Authentication/loginscreen.dart
@@ -39,114 +39,105 @@ class _LoginScreen extends State<LoginScreen> {
     return SafeArea(
       child: Scaffold(
         body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-
-              const Text("Login to TiK Tok",
-              style: TextStyle(
-                  fontSize: 26, fontWeight: FontWeight.bold
-              ),),
-
-              Container(
-                  margin: const EdgeInsets.only(top: 20,bottom: 20),
-                 child: const Text("Manage your account, check notifications, \ncomment on videos, and more",
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 100),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(padding: const EdgeInsets.fromLTRB(0,0,0,20),
+                  child: Image.asset("images/tiktok.png", height: 100,),
+                ),
+                const Text("Login to TiK Tok",
                 style: TextStyle(
-                    fontSize: 16,color: Colors.grey
-                ),)),
+                    fontSize: 26, fontWeight: FontWeight.bold
+                ),),
 
-              // phone login button
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-               child: Column(
-                 children: [
-                   ElevatedButton(onPressed: (){
-                     Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginWithPhone()));
+                Container(
+                    margin: const EdgeInsets.only(top: 10,bottom: 20),
+                   child: const Text("Manage your account, check notifications, \ncomment on videos, and more",
+                  style: TextStyle(
+                      fontSize: 16,color: Colors.grey,
+                  ),)),
 
-                   },style: ElevatedButton.styleFrom(
-                     backgroundColor: Colors.grey
-                   ), child: const Padding(
-                     padding:  EdgeInsets.all(15.0),
-                     child: Row(
-                       children: [
-                         Icon(Icons.phone,color: Colors.black,),
-                         SizedBox(width: 20,),
-                         Text("Login with phone number",
-                           style: TextStyle(
-                               fontSize: 14, color: Colors.black),)
-                       ],
-                     ),
-                   ))
-                 ],
-               ),
-              ),
+                // phone login button
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                 child: Column(
+                   children: [
+                     ElevatedButton(onPressed: (){
+                       Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginWithPhone()));
 
-              // email login button
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  children: [
-                    const SizedBox(height: 20),
-                    ElevatedButton(onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginWithEmail()));
-                    },style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey
-                    ), child: const Padding(
-                      padding:  EdgeInsets.all(15.0),
-                      child: Row(
-                        children: [
-                          Icon(Icons.email,color: Colors.black,),
-                          SizedBox(width: 20,),
-                          Text("Login with Email",
-                            style: TextStyle(
-                                fontSize: 14, color: Colors.black),)
-                        ],
-                      ),
-                    ))
-                  ],
+                     },style: ElevatedButton.styleFrom(
+                       backgroundColor: Colors.grey
+                     ), child: const Padding(
+                       padding:  EdgeInsets.all(15.0),
+                       child: Row(
+                         children: [
+                           Icon(Icons.phone,color: Colors.black,),
+                           SizedBox(width: 20,),
+                           Text("Login with phone number",
+                             style: TextStyle(
+                                 fontSize: 14, color: Colors.black),)
+                         ],
+                       ),
+                     ))
+                   ],
+                 ),
                 ),
-              ),
 
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  children: [
-                    const SizedBox(height: 20),
-                    ElevatedButton(onPressed: (){
-                      signInWithGoogle();
-                    },style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey
-                    ), child: const Padding(
-                      padding:  EdgeInsets.all(15.0),
-                      child: Row(
-                        children: [
-                          Icon(Icons.email,color: Colors.black,),
-                          SizedBox(width: 20,),
-                          Text("Login with Google",
-                            style: TextStyle(
-                                fontSize: 14, color: Colors.black),)
-                        ],
-                      ),
-                    ))
-                  ],
+                // email login button
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 20),
+                      ElevatedButton(onPressed: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => LoginWithEmail()));
+                      },style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.grey
+                      ), child: const Padding(
+                        padding:  EdgeInsets.all(15.0),
+                        child: Row(
+                          children: [
+                            Icon(Icons.email,color: Colors.black,),
+                            SizedBox(width: 20,),
+                            Text("Login with Email",
+                              style: TextStyle(
+                                  fontSize: 14, color: Colors.black),)
+                          ],
+                        ),
+                      ))
+                    ],
+                  ),
                 ),
-              ),
 
-              const SizedBox(height: 20,),
-              GestureDetector(
-                onTap: (){
-
-                },
-                child:const Text(
-                "Don't have an account? Sign up.",
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.black
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 20),
+                      ElevatedButton(onPressed: (){
+                        signInWithGoogle();
+                      },style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.grey
+                      ), child: const Padding(
+                        padding:  EdgeInsets.all(15.0),
+                        child: Row(
+                          children: [
+                            Icon(Icons.public,color: Colors.black,),
+                            SizedBox(width: 20,),
+                            Text("Login with Google",
+                              style: TextStyle(
+                                  fontSize: 14, color: Colors.black),)
+                          ],
+                        ),
+                      ))
+                    ],
+                  ),
                 ),
-                textAlign: TextAlign.center,),
-              )
-            ],
+              ],
+            ),
           ),
         ),
       ),
