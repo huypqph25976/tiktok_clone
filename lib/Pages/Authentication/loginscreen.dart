03e1e5ff3ec5593bdab78747ce8b210f90037e35
bfjshfbh <nhatltph28401@fpt.edu.vn>
2023-10-13 09:29:02 +0700

    nhat

--- lib/Pages/Authentication/loginscreen.dart
+++ lib/Pages/Authentication/loginscreen.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:tiktok_clone2/Pages/Authentication/loginwithemail.dart';
 import 'package:tiktok_clone2/Pages/Authentication/loginwithphone.dart';
+import 'package:tiktok_clone2/Pages/Home/homeScreen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,6 +13,27 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreen extends State<LoginScreen> {
+
+  signInWithGoogle() async {
+
+    GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+
+    AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken
+    );
+
+    UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+    print(userCredential.user?.displayName);
+    if(userCredential.user != null){
+      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>HomeScreen(),),);
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -20,7 +44,7 @@ class _LoginScreen extends State<LoginScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
 
-              const Text("Login to Tick Tock",
+              const Text("Login to TiK Tok",
               style: TextStyle(
                   fontSize: 26, fontWeight: FontWeight.bold
               ),),
@@ -75,6 +99,31 @@ class _LoginScreen extends State<LoginScreen> {
                           Icon(Icons.email,color: Colors.black,),
                           SizedBox(width: 20,),
                           Text("Login with Email",
+                            style: TextStyle(
+                                fontSize: 14, color: Colors.black),)
+                        ],
+                      ),
+                    ))
+                  ],
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    ElevatedButton(onPressed: (){
+                      signInWithGoogle();
+                    },style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey
+                    ), child: const Padding(
+                      padding:  EdgeInsets.all(15.0),
+                      child: Row(
+                        children: [
+                          Icon(Icons.email,color: Colors.black,),
+                          SizedBox(width: 20,),
+                          Text("Login with Google",
                             style: TextStyle(
                                 fontSize: 14, color: Colors.black),)
                         ],
