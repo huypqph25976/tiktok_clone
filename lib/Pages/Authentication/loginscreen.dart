1fa382b19887294da36918181244da5c195c7f84
nguyenhuuthong <thonglaanh1011@gmail.com>
2023-10-13 11:15:43 +0700

    Firt commit

--- lib/Pages/Authentication/loginscreen.dart
+++ lib/Pages/Authentication/loginscreen.dart
@@ -19,43 +19,54 @@ class _LoginScreen extends State<LoginScreen> {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-
-              const Text("Login to Tick Tock",
-              style: TextStyle(
-                  fontSize: 26, fontWeight: FontWeight.bold
-              ),),
+              const Text(
+                "Login to Tick Tock",
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+              ),
 
               Container(
-                  margin: const EdgeInsets.only(top: 20,bottom: 20),
-                 child: const Text("Manage your account, check notifications, \ncomment on videos, and more",
-                style: TextStyle(
-                    fontSize: 16,color: Colors.grey
-                ),)),
+                  margin: const EdgeInsets.only(top: 20, bottom: 20),
+                  child: const Text(
+                    "Manage your account, check notifications, \ncomment on videos, and more",
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  )),
 
               // phone login button
               Container(
                 margin: const EdgeInsets.symmetric(horizontal: 20),
-               child: Column(
-                 children: [
-                   ElevatedButton(onPressed: (){
-                     Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginWithPhone()));
-
-                   },style: ElevatedButton.styleFrom(
-                     backgroundColor: Colors.grey
-                   ), child: const Padding(
-                     padding:  EdgeInsets.all(15.0),
-                     child: Row(
-                       children: [
-                         Icon(Icons.phone,color: Colors.black,),
-                         SizedBox(width: 20,),
-                         Text("Login with phone number",
-                           style: TextStyle(
-                               fontSize: 14, color: Colors.black),)
-                       ],
-                     ),
-                   ))
-                 ],
-               ),
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const LoginWithPhone()));
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.grey),
+                        child: const Padding(
+                          padding: EdgeInsets.all(15.0),
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.phone,
+                                color: Colors.black,
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Text(
+                                "Login with phone number",
+                                style: TextStyle(
+                                    fontSize: 14, color: Colors.black),
+                              )
+                            ],
+                          ),
+                        ))
+                  ],
+                ),
               ),
 
               // email login button
@@ -64,49 +75,53 @@ class _LoginScreen extends State<LoginScreen> {
                 child: Column(
                   children: [
                     const SizedBox(height: 20),
-                    ElevatedButton(onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginWithEmail()));
-                    },style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey
-                    ), child: const Padding(
-                      padding:  EdgeInsets.all(15.0),
-                      child: Row(
-                        children: [
-                          Icon(Icons.email,color: Colors.black,),
-                          SizedBox(width: 20,),
-                          Text("Login with Email",
-                            style: TextStyle(
-                                fontSize: 14, color: Colors.black),)
-                        ],
-                      ),
-                    ))
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginWithEmail()));
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.grey),
+                        child: const Padding(
+                          padding: EdgeInsets.all(15.0),
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.email,
+                                color: Colors.black,
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Text(
+                                "Login with Email",
+                                style: TextStyle(
+                                    fontSize: 14, color: Colors.black),
+                              )
+                            ],
+                          ),
+                        ))
                   ],
                 ),
               ),
 
-              const SizedBox(height: 20,),
+              const SizedBox(
+                height: 20,
+              ),
               GestureDetector(
-                onTap: (){
-
-                },
-                child:const Text(
-                "Don't have an account? Sign up.",
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.black
+                onTap: () {},
+                child: const Text(
+                  "Don't have an account? Sign up.",
+                  style: TextStyle(fontSize: 12, color: Colors.black),
+                  textAlign: TextAlign.center,
                 ),
-                textAlign: TextAlign.center,),
               )
             ],
           ),
         ),
       ),
-          
     );
-
-
-
   }
-
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,22 @@ import 'package:tiktok_clone2/Pages/Authentication/loginscreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:tiktok_clone2/firebase_options.dart';
 
- Future<void>main() async {
-   WidgetsFlutterBinding.ensureInitialized();
-   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+Future<void> main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+  WidgetsFlutterBinding.ensureInitialized(); //Check ứng dụng hoạt động đúng
+  // Khởi tạo cấu hình cung cập cho firebase
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyD8xSXM3WMc8a11u1Dp4Dco8XJjpbawUnw',
+      appId: '1:117938042068:android:2c128039076c3084f9b741',
+      messagingSenderId: '117938042068',
+      projectId: 'tiktok-clone-4fde1',
+    ),
+  );
   runApp(const MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -19,16 +30,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tick Tock',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-
-
-
-      ),
+      theme: ThemeData(),
       home: const LoginScreen(),
     );
-
-
   }
 }
-
-
