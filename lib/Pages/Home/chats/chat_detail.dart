d2b8ae920b2be420264440adf3015a16358ad077
huy <118325984+Tkmizu@users.noreply.github.com>
2023-10-27 12:04:58 +0700

    24 - chat detail

--- lib/Pages/Home/chats/chat_detail.dart
+++ lib/Pages/Home/chats/chat_detail.dart
@@ -1,14 +1,19 @@
 import 'dart:io';
 
+import 'package:chat_bubbles/chat_bubbles.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
+import 'package:tiktok_clone2/Pages/Home/UserPage/PersonInfomation.dart';
 
 import '../../../Providers/loading_model.dart';
 import '../../../Providers/save_model.dart';
 import '../../../Services/chatService.dart';
+import '../../../Services/storageService.dart';
+import '../../../Widgets/customText.dart';
 
 class ChatDetail extends StatefulWidget {
   final String personID;
@@ -106,10 +111,259 @@ class _ChatDetailState extends State<ChatDetail> {
 
   @override
   Widget build(BuildContext context) {
+    // context.read<LoadingModel>().isLoading = false;
+    // context.read<SaveModel>().isSaving = 0.0;
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 50,
+              width: 50,
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            PersonInformation(personID: personID)),
+                  );
+                },
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(personImage),
+                ),
+              ),
+            ),
 
-    context.read<LoadingModel>().isLoading = false;
-    context.read<SaveModel>().isSaving = 0.0;
+            const SizedBox(
+              width: 15,
+            ),
 
-    return const Placeholder();
+            Container(
+              child: CustomText(
+                alignment: Alignment.center,
+                fontsize: 20,
+                text: personUsername,
+                fontFamily: 'Poppins',
+                color: Colors.white,
+              ),
+            ),
+
+
+          ],
+        ),
+
+        elevation: 0,
+        toolbarHeight: 80,
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('chats')
+            .doc(chatID)
+            .collection('messages')
+            .orderBy('createdOn', descending: true)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if(snapshot.hasError){
+            return const Text('Something went wrong');
+          }
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if(snapshot.hasData){
+            return Column(
+              children: [
+                Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black87.withOpacity(0.2),
+                            spreadRadius: 1,
+                            blurRadius: 2,
+                            offset: const Offset(
+                                1, 1), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: ListView(
+                          reverse: true,
+                          children: snapshot.data!.docs.map((DocumentSnapshot document){
+                            var data = document.data()! as Map<String, dynamic>;
+                            return Padding(
+                              padding: const EdgeInsets.only(top: 20.0),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Expanded(
+                                          child: data['type'] == 'image' ? GestureDetector(onLongPress: (){},
+                                          child: Container(),
+                                          ) : BubbleSpecialThree(
+                                              text: data['content'],
+                                              color: isSender(data['uID'].toString()) ? Colors.blue : Colors.grey,
+                                              tail: true,
+                                              isSender: isSender(data['uID'].toString()),
+                                              textStyle: const TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 16,
+                                              ),
+
+                                          ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(
+                                    height: 8,
+                                  ),
+
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        data['createdOn'] == null
+                                            ? DateTime.now().toString()
+                                            : DateFormat.yMMMd()
+                                            .add_jm()
+                                            .format(
+                                            data['createdOn'].toDate()),
+                                        style: const TextStyle(
+                                            color: Colors.black54,
+                                            fontSize: 12),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+
+                    ),
+                ),
+                // Consumer<LoadingModel>(
+                //   builder: (_, isLoadingImage, __) {
+                //     if (isLoadingImage.isLoading) {
+                //       return Container(
+                //         width: MediaQuery.of(context).size.width,
+                //         color: Colors.white,
+                //         child: const CircleAvatar(
+                //           backgroundColor: Colors.white,
+                //           child: Center(
+                //             child: CircularProgressIndicator(),
+                //           ),
+                //         ),
+                //       );
+                //     } else {
+                //       return Container();
+                //     }
+                //   },
+                // ),
+
+                Container(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                            onPressed: () async {
+                              File? image = await getImage(ImageSource.camera);
+                              // if (image == null) {
+                              //   context.read<LoadingModel>().changeLoading();
+                              // } else {
+                                String fileName =
+                                await StorageService.uploadImage(
+                                    image);
+                                sendMessage(fileName, personID, 'image');
+                                try {
+                                  context.read<LoadingModel>().changeLoading();
+                                } catch (e) {}
+                              // }
+
+                            }, icon: Icon(Icons.enhance_photo_translate, color: Colors.black,)),
+
+                        IconButton(
+                            onPressed: () async {
+                              // context.read<LoadingModel>().changeLoading();
+                              File? fileImage =
+                              await getImage(ImageSource.gallery);
+                              // if (fileImage == null) {
+                              //   context.read<LoadingModel>().changeLoading();
+                              // } else {
+                                String fileName =
+                                await StorageService.uploadImage(
+                                    fileImage);
+                                sendMessage(fileName, personID, 'image');
+                                try {
+                                  context
+                                      .read<LoadingModel>()
+                                      .changeLoading();
+                                } catch (e) {}
+                              // }
+                            },
+                            icon: Icon(Icons.image_outlined,
+                                color: Colors.black)),
+
+                        Expanded(
+                          child: Container(
+                            height: 45,
+                            child: TextField(
+                              controller: _textEditingController,
+                              textAlignVertical: TextAlignVertical.bottom,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    width: 2,
+                                    color: Colors.black,
+                                  ),
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                                hintText: "Type here ...",
+                                suffixIcon: IconButton(
+                                  onPressed: () {
+                                    sendMessage(_textEditingController.text,
+                                        personID, 'text');
+                                  },
+                                  icon: Icon(
+                                    Icons.send_rounded,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }
+          return const Center(
+            child: Text('Failed!'),
+          );
+        },
+      ),
+
+    );
   }
 }
